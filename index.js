24b1386ca33d438f9f880b91847a7f3e4223f753
Faith Wangari <njihiafaith@gmail.com>
2025-03-28 15:08:51 +0300

    Adds to index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+return Math.abs(someValue - 42); 
+}
+function distanceFromHqInFeet(someValue) {
+   const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled=Math.abs(destination - start)
+    return blocksTravelled * 264
+}
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+    if (distanceInFeet <= 400) {
+        return 0; // First 400 feet are free
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02; // 2 cents per foot over 400 feet
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25; // Flat fare for distances between 2000 and 2500 feet
+    } else {
+        return 'cannot travel that far'; // No rides over 2500 feet
+    }
+    }
+    
